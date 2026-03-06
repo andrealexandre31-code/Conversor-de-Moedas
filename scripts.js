0c0457c6760b88f8115487f54c24c5346cdc75a6
andrealexandre31-cod <andrealexandre31@gmail.com>
2026-03-06 11:28:27 -0300

    Atualizar a interface do conversor de moedas: alterar títulos, adicionar estilos e otimizar a lógica de conversão com dados dinâmicos da API.

--- scripts.js
+++ scripts.js
@@ -2,20 +2,21 @@
 const convertButton = document.querySelector(".convert-Button")
 const currencySelect = document.querySelector(".currency-select")       
 
-function convertValues () {
+async function convertValues () {
 
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
     const currencyValueConverted = document.querySelector(".currency-value-converted")
     const currencyValueToConvertEuro = document.querySelector(".euro")
     const currencyValueToConvertBtc = document.querySelector(".btc")
+
     
-    console.log(currencySelect.value)
+    const data = await fetch("https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL")
+    .then(res => res.json())
 
-
-    const dolarToday = 5.42
-    const euroToday = 6.37
-    const btcToday = 86.025
+    const dolarToday = data.USDBRL.high
+    const euroToday = data.EURBRL.high
+    const btcToday = data.BTCBRL.high
 
 
     if (currencySelect.value == "dolar") {
@@ -32,13 +33,12 @@ function convertValues () {
              .format(inputCurrencyValue / euroToday)
     }
 
-    if (currencySelect.value == "BTC") {
-        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-            style: "currency",
-             currency: "BTC"})
-             .format(inputCurrencyValue / btcToday)
-    }
-
+  if (currencySelect.value === "BTC") {
+      const valueInBtc = inputCurrencyValue / btcToday;
+      const formattedBtc = valueInBtc.toFixed(8);
+      currencyValueConverted.innerHTML = `₿ ${formattedBtc}`;
+  
+}
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
          currency: "BRL"})
